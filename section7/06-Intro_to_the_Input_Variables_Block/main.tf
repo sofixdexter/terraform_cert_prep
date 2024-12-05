92abd9c789727ed418b60d4a2002311543faeb00
soufiane ESSABONI <s.essaboni@hotmail.fr>
2024-12-05 22:57:38 +0100

    add lab 6 to the 7th section

---
+++ section7/06-Intro_to_the_Input_Variables_Block/main.tf
@@ -0,0 +1,31 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+#Retrieve the list of AZs in the current AWS region
+data "aws_availability_zones" "available" {}
+data "aws_region" "current" {}
+
+#Define the VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name        = var.vpc_name
+    Environment = "demo_environment"
+    Terraform   = "true"
+  }
+}
+
+resource "aws_subnet" "variables-subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.variables_sub_cidr
+  availability_zone       = var.variables_sub_az
+  map_public_ip_on_launch = var.variables_sub_auto_ip
+
+  tags = {
+    Name      = "sub-variables-${var.variables_sub_az}"
+    Terraform = "true"
+  }
+}
