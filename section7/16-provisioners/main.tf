f04c80bab828bba9017f53b5447b24912758af9a
Soufiane ESSABONI <s.essaboni@hotmail.fr>
2024-12-13 10:49:51 +0100

    add stuff

--- section7/16-provisioners/main.tf
+++ section7/16-provisioners/main.tf
@@ -79,3 +79,25 @@ resource "aws_key_pair" "generated" {
     ignore_changes = [key_name]
   }
 }
+
+# Security Groups
+
+resource "aws_security_group" "ingress-ssh" {
+  name   = "allow-all-ssh"
+  vpc_id = aws_vpc.vpc.id
+  ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+  }
+  // Terraform removes the default rule
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
