eeb36551f4b0b9961ba64bc02760beccc5eaef7d
Soufiane Essaboni <s.essaboni@hotmail.fr>
2024-12-24 09:01:15 +0100

    add a module from public registry

--- section9/01-local_modules/main.tf
+++ section9/01-local_modules/main.tf
@@ -309,3 +309,26 @@ output "public_ip_srv2" {
 output "public_dns_srv2" {
   value = module.server2.public_dns
 }
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.0.1"
+  name = "myasg"
+  vpc_zone_identifier = [aws_subnet.private_subnets["private_subnet_1"].id, 
+  aws_subnet.private_subnets["private_subnet_2"].id, 
+  aws_subnet.private_subnets["private_subnet_3"].id]
+  min_size            = 0
+  max_size            = 1
+  desired_capacity    = 1
+
+# Launch template
+  use_lt    = true
+  create_lt = true
+
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  
+  tags_as_map = {
+    Name = "Web EC2 Server 2"
+  }
+}
