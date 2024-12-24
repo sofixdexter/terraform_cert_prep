76d32efa95d22b96d1ba8efc01ebe95348dc9260
Soufiane Essaboni <s.essaboni@hotmail.fr>
2024-12-24 13:40:58 +0100

    add refactor lab to the 9th section

---
+++ section9/02-refactor_modules/terraform.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.1.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
+  }
+}
