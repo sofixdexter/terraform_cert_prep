709c947d5a20174deb4b27fefa2921c180752e80
Soufiane Essaboni <s.essaboni@hotmail.fr>
2024-12-24 08:22:51 +0100

    add the local server module

--- section9/01-local_modules/main.tf
+++ section9/01-local_modules/main.tf
@@ -312,3 +312,14 @@ resource "aws_instance" "web_server" {
   }
 
 }
+
+module "server" {
+  source          = "./server"
+  ami             = data.aws_ami.ubuntu.id
+  subnet_id       = aws_subnet.public_subnets["public_subnet_3"].id
+  security_groups = [
+    aws_security_group.vpc-ping.id,
+    aws_security_group.ingress-ssh.id,
+    aws_security_group.vpc-web.id
+  ]
+}
