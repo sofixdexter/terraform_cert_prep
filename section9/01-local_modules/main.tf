17493cb93b57daaa1d2df4a90b9dd43ed68cced8
Soufiane Essaboni <s.essaboni@hotmail.fr>
2024-12-24 09:35:32 +0100

    remove some useless argument

--- section9/01-local_modules/main.tf
+++ section9/01-local_modules/main.tf
@@ -323,7 +323,6 @@ module "autoscaling" {
 
 # Launch template
   create_launch_template = true
-  iam_instance_profile = "ecsInstanceRole"
   image_id      = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
