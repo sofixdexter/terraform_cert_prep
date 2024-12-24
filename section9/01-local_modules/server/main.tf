3cda4c3150eacada2f71ab39e5d69e7b8a36c222
Soufiane Essaboni <s.essaboni@hotmail.fr>
2024-12-24 08:13:16 +0100

    add the local server module

--- section9/01-local_modules/server/main.tf
+++ section9/01-local_modules/server/main.tf
@@ -270,13 +270,45 @@ output "server_url" {
   description = "The URL to access the web server"
 }
 
-module "server" {
-  source          = "./server"
-  ami             = data.aws_ami.ubuntu.id
-  subnet_id       = aws_subnet.public_subnets["public_subnet_3"].id
-  security_groups = [
-    aws_security_group.vpc-ping.id,
-    aws_security_group.ingress-ssh.id,
-    aws_security_group.vpc-web.id
-  ]
+output "hello_mehdi" {
+  value = "fin a sat ha page lanciha"
+  description = "The URL to access the web server"
+}
+
+# Terraform Resource Block - To Build Web Server in Public Subnet
+resource "aws_instance" "web_server" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups             = [aws_security_group.vpc-ping.id, 
+     aws_security_group.ingress-ssh.id, aws_security_group.vpc-web.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.generated.key_name
+  connection {
+    user        = "ubuntu"
+    private_key = tls_private_key.generated.private_key_pem
+    host        = self.public_ip
+  }
+
+  # Leave the first part of the block unchanged and create our `local-exec` provisioner
+  provisioner "local-exec" {
+    command = "chmod 600 ${local_file.private_key_pem.filename}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo rm -rf /tmp",
+      "sudo git clone https://github.com/hashicorp/demo-terraform-101 /tmp",
+      "sudo sh /tmp/assets/setup-web.sh",
+    ]
+  }
+
+  tags = {
+    Name = "Web EC2 Server"
+  }
+
+  lifecycle {
+    ignore_changes = [security_groups]
+  }
+
 }
