57496054740d99bb98cc48236512efd4ea9041b8
Soufiane Essaboni <s.essaboni@hotmail.fr>
2024-12-24 08:50:27 +0100

    add some proprties

--- section9/01-local_modules/main.tf
+++ section9/01-local_modules/main.tf
@@ -293,12 +293,13 @@ output "public_dns_srv1" {
 module "server2" {
   source          = "./modules/web_server"
   ami             = data.aws_ami.ubuntu.id
+  key_name        = aws_key_pair.generated.key_name
+  user            = "ubuntu"
+  private_key     = tls_private_key.generated.private_key_pem
   subnet_id       = aws_subnet.public_subnets["public_subnet_1"].id
-  security_groups = [
-    aws_security_group.vpc-ping.id,
-    aws_security_group.ingress-ssh.id,
-    aws_security_group.vpc-web.id
-  ]
+  security_groups = [aws_security_group.vpc-ping.id, 
+  aws_security_group.ingress-ssh.id, 
+  aws_security_group.vpc-web.id]
 }
 
 output "public_ip_srv2" {
